56492054b410d97d289caa0adf3c86ff49f2af94
Peter Walter Tafirenyika Chifamba <PeterWalter@users.noreply.github.com>
2026-05-18 08:05:27 +0200

    "add-resizable-splitter-panels-docking-layout"

--- LabelDesigner.App/MainWindow.xaml.cs
+++ LabelDesigner.App/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ namespace LabelDesigner.App;
 public sealed partial class MainWindow : Window
 {
     public MainViewModel ViewModel { get; }
+    private bool _isDraggingSplitter;
+    private bool _isLeftSplitter;
+    private double _startPointerX;
 
     public MainWindow(MainViewModel vm)
     {
@@ -15,15 +18,11 @@ public sealed partial class MainWindow : Window
         ViewModel = vm;
         RootGrid.DataContext = ViewModel;
 
-        // Initial layers panel refresh
         ViewModel.Designer.Layers.Refresh();
 
-        // Set default window size (WinUI 3 Window doesn't support Width/Height in XAML)
-        Microsoft.UI.Windowing.AppWindow appWindow = this.AppWindow;
+        var appWindow = this.AppWindow;
         if (appWindow != null)
-        {
             appWindow.Resize(new Windows.Graphics.SizeInt32(1400, 900));
-        }
     }
 
     private void OnLayerItemClick(object sender, PointerRoutedEventArgs e)
@@ -34,5 +33,45 @@ public sealed partial class MainWindow : Window
             ViewModel.Designer.Layers.SelectElement(evm.ElementId);
             ViewModel.Designer.RequestRedraw?.Invoke();
         }
+    }
+
+    private void OnSplitterPressed(object sender, PointerRoutedEventArgs e)
+    {
+        _isDraggingSplitter = true;
+        _isLeftSplitter = sender == SplitterLeft;
+        _startPointerX = e.GetCurrentPoint(RootGrid).Position.X;
+        (sender as UIElement)?.CapturePointer(e.Pointer);
+    }
+
+    private void OnSplitterMoved(object sender, PointerRoutedEventArgs e)
+    {
+        if (!_isDraggingSplitter) return;
+        var currentX = e.GetCurrentPoint(RootGrid).Position.X;
+        var delta = currentX - _startPointerX;
+
+        if (_isLeftSplitter)
+        {
+            var newWidth = LayerColumn.Width.Value + delta;
+            if (newWidth > 60 && newWidth < 500)
+            {
+                LayerColumn.Width = new GridLength(newWidth);
+                _startPointerX = currentX;
+            }
+        }
+        else
+        {
+            var newWidth = PropertiesColumn.Width.Value - delta;
+            if (newWidth > 120 && newWidth < 500)
+            {
+                PropertiesColumn.Width = new GridLength(newWidth);
+                _startPointerX = currentX;
+            }
+        }
+    }
+
+    private void OnSplitterReleased(object sender, PointerRoutedEventArgs e)
+    {
+        _isDraggingSplitter = false;
+        (sender as UIElement)?.ReleasePointerCapture(e.Pointer);
     }
 }
